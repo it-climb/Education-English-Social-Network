9b03561c17cafeda41390db8bec09617ae9d807b
agios <agios987@gmail.com>
2016-02-11 21:11:57 +0200

    alpha 0.1

--- src/main/java/evg/testt/util/converter/EmailConverter.java
+++ src/main/java/evg/testt/util/converter/EmailConverter.java
@@ -1,24 +1,46 @@
 package evg.testt.util.converter;
 
+
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.security.Key;
+
 
 @Converter
 public class EmailConverter implements AttributeConverter<String, String> {
 
+    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final byte[] KEY = "MySuperSecretKey".getBytes();
+
     @Override
     public String convertToDatabaseColumn(String email) {
-        StringBuilder sb = new StringBuilder ();
-        sb.append(email).append("111");
-        return sb.toString();
+        // do some encryption
+        Key key = new SecretKeySpec(KEY, "AES");
+        try {
+            Cipher c = Cipher.getInstance(ALGORITHM);
+            c.init(Cipher.ENCRYPT_MODE, key);
+            return Base64.encodeBase64URLSafeString(c.doFinal(email.getBytes()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public String convertToEntityAttribute(String emailString) {
-
-        String[] email = emailString.split("111");
-
-        return email[0];
+    public String convertToEntityAttribute(String dbData) {
+        // do some decryption
+        Key key = new SecretKeySpec(KEY, "AES");
+        try {
+            Cipher c = Cipher.getInstance(ALGORITHM);
+            c.init(Cipher.DECRYPT_MODE, key);
+            return new String(c.doFinal(Base64.decodeBase64(dbData)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
