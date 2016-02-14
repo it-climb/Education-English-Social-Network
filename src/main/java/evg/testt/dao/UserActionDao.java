9c618b2d84aa99c4c4f08dbff8e13b522e29957f
Alex-Kobets <renbrane@gmail.com>
2016-02-14 18:19:21 +0200

    Added alpha version of user statistics

---
+++ src/main/java/evg/testt/dao/UserActionDao.java
@@ -0,0 +1,28 @@
+package evg.testt.dao;
+
+import evg.testt.model.UserAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserActionDao {
+
+    @Autowired
+    private MongoOperations mongoOperations;
+
+    public void save(UserAction userAction) {
+        mongoOperations.save(userAction);
+    }
+
+    public UserAction get (Long id) {
+        return mongoOperations.findOne(Query.query(Criteria.where("id").is(id)), UserAction.class);
+    }
+    public List<UserAction> getAll() { return mongoOperations.findAll(UserAction.class); }
+
+    public void remove(Long id) { mongoOperations.remove(Query.query(Criteria.where("id").is(id)), UserAction.class); }
+}
---
+++ src/main/java/evg/testt/service/UserActionService.java
@@ -0,0 +1,30 @@
+package evg.testt.service;
+
+
+import evg.testt.dao.SequenceDao;
+import evg.testt.dao.UserActionDao;
+import evg.testt.model.UserAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserActionService {
+
+    @Autowired
+    private SequenceDao sequenceDao;
+    @Autowired private UserActionDao userActionDao;
+
+    public void save(UserAction userAction){
+        if(userAction.getId() == null) {
+            userAction.setId(sequenceDao.getNextSequenceId(userAction.ACTION_NAME));
+        }
+        userActionDao.save(userAction);
+    }
+    public UserAction get(Long id) { return userActionDao.get(id); }
+
+    public List<UserAction> getAll() { return userActionDao.getAll(); }
+
+    public  void remove(Long id) {userActionDao.remove(id);}
+}
