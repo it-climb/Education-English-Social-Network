92ebcbce0393c251b0ecc0d6496c894b83d5fe4f
Sergei Bragin <mr.sergei.bragin@gamil.com>
2016-02-16 08:07:41 +0200

    Test2

--- src/main/java/evg/testt/util/userActions/UserActionAOP.java
+++ src/main/java/evg/testt/util/userActions/UserActionAOP.java
@@ -1,11 +1,14 @@
 package evg.testt.util.userActions;
 
 import evg.testt.model.User;
+import evg.testt.service.UserService;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,15 +17,18 @@ import javax.servlet.http.HttpSession;
 @Aspect
 @Component
 public class UserActionAOP {
+
+    @Autowired
+    UserService userService;
+
     public String superMagic(HttpServletRequest request){
         HttpSession session = request.getSession();
         User sessionUser = (User) session.getAttribute("user");
-        if(sessionUser==null){
-            return "Guest";
-        }else {
-            String userInfo = sessionUser.getEmail();
-            return userInfo;
-        }
+        if(sessionUser!=null){
+            String userEmail = sessionUser.getEmail();
+            User user = userService.getByEmail(userEmail);
+            return "Id user:"+user.getId()+" User email: "+user.getEmail();
+        }else return "Guest";
     }
 
     @Before("execution(* evg.testt.controller.UserController.show(..))")
@@ -31,7 +37,7 @@ public class UserActionAOP {
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
-    @Around("execution(* evg.testt.controller.DepartmentController.addNewOne(..))")
+   /* @Around("execution(* evg.testt.controller.DepartmentController.addNewOne(..))")
     public void magic(ProceedingJoinPoint joinPoint) {
         System.out.println("BEFORE!");
         try {
@@ -43,5 +49,10 @@ public class UserActionAOP {
         }
         System.out.println("AFTER!");
 
+    }*/
+    @After("execution(* evg.testt.controller.DepartmentController.addNewOne(..))")
+    public void afterMagic(JoinPoint joinPoint){
+        Object[]arr = joinPoint.getArgs();
+        System.out.println(superMagic((HttpServletRequest) arr[3]));
     }
 }
