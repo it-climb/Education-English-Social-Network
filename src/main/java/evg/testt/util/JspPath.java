5d1dacc0d99e67d49ed4681b9060f7b70e079974
Danil-Lapirow <fergon4ik@gmail.com>
2016-02-21 17:45:18 +0200

    merged feature/65 with dev

--- src/main/java/evg/testt/util/JspPath.java
+++ src/main/java/evg/testt/util/JspPath.java
@@ -6,26 +6,6 @@ package evg.testt.util;
 public class JspPath {
 
     public static final String HOME = "home";
-    public static final String DEPARTMENT_ALL = "allDep";
-    public static final String DEPARTMENT_EDIT = "depEdit";
-    public static final String EMPLOYEE_ALL = "allEmp";
-    public static final String EMPLOYEE_EDIT = "editEmp";
-    public static final String TRANSLATOR_HOME = "transl";
-    public static final String USER_LOGIN = "login";
-    public static final String USER_REGISTRATION = "registration";
-    public static final String USER_LOGI_PROBLEM = "logProblem"; // Беда в слове LOGIN
-//    public static final String DESCRIPTION_ALL = "descAll";
-//    public static final String DESCRIPTION_EDIT = "descEdit";
-    public static final String WORD_ALL = "words/all";
-    public static final String WORD_EDIT = "words/edit";
-    public static final String ACCOUNT_SHOW = "account/show";
-    public static final String DEFAULT_ERROR_VIEW = "error-404";
-    public static final String DESCRIPTION_ALL = "descriptions/all";
-    public static final String DESCRIPTION_EDIT = "descriptions/edit";
-    public static final String CHAT_ALL = "chat/all";
-    public static final String MESSAGES = "messages/all";
-    public static final String POINTS_ALL = "points/all";
-
     public static final String DEPARTMENT_ALL = "/departments/all";
     public static final String DEPARTMENT_EDIT = "/departments/edit";
     public static final String EMPLOYEE_ALL = "/employees/all";
@@ -39,7 +19,6 @@ public class JspPath {
     public static final String WORD_ALL = "/words/all";
     public static final String WORD_EDIT = "/words/edit";
     public static final String ACCOUNT_SHOW = "/account/show";
-
     public static final String DEFAULT_ERROR_VIEW = "error-404"; //page not found
     public static final String ISE_ERROR_VIEW = "ErrorNeedLogin"; //internal server error
     public static final String CHAT_ALL = "/chat/all";
