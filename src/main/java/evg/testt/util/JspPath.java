9c3767d0c06a9ffdc1c1fadbc80e9786411df072
Alex-Kobets <renbrane@gmail.com>
2016-02-21 11:49:32 +0200

    Deleted header and footer from /regSave Confirm password with stars

--- src/main/java/evg/testt/util/JspPath.java
+++ src/main/java/evg/testt/util/JspPath.java
@@ -13,7 +13,7 @@ public class JspPath {
     public static final String EMPLOYEE_EDIT = "/employees/edit";
     public static final String TRANSLATOR_HOME = "/translator/translate";
     public static final String USER_LOGIN = "/users/login";
-    public static final String USER_REGISTRATION = "/users/registration";
+    public static final String USER_REGISTRATION = "users/registration";
     public static final String USER_LOGIN_PROBLEM = "/users/loginProblems";
     public static final String DESCRIPTION_ALL = "/descriptions/all";
     public static final String DESCRIPTION_EDIT = "/descriptions/edit";
