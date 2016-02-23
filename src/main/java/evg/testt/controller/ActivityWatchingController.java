87f1516d7c7b47a844806f138ee8e86aade06329
sergei-bragin <serbragin1@gmail.com>
2016-02-23 22:53:07 +0200

    Preview

--- src/main/java/evg/testt/controller/ActivityWatchingController.java
+++ src/main/java/evg/testt/controller/ActivityWatchingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class WatchingActivityController {
+public class ActivityWatchingController {
 
     //TODO Service level
 
