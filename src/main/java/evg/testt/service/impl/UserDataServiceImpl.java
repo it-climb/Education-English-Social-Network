acd343afd25052c9f85250748923944afe975e6a
Alexander-Ansimov <alex.ansimov.ua@gmail.com>
2016-03-06 13:05:56 +0200

    make functionality for adding KnowledgeLevelUnits to user data in UserDataService

--- src/main/java/evg/testt/service/impl/UserDataServiceImpl.java
+++ src/main/java/evg/testt/service/impl/UserDataServiceImpl.java
@@ -1,13 +1,17 @@
 package evg.testt.service.impl;
 
 import evg.testt.dao.UserDataDao;
+import evg.testt.model.KnowledgeLevelUnits;
 import evg.testt.model.User;
 import evg.testt.model.UserData;
 import evg.testt.service.UserDataService;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class UserDataServiceImpl extends BaseService<UserData, UserDataDao> implements UserDataService{
+
     public UserData findByUser(User user) {
          UserData userData = dao.findByUser(user);
         if(userData != null) {
@@ -18,5 +22,13 @@ public class UserDataServiceImpl extends BaseService<UserData, UserDataDao> impl
             dao.save(userData);
             return userData;
         }
+    }
+
+    public void addKnowledgeLevelUnits(UserData userData, Set<KnowledgeLevelUnits> knowledgeLevelUnitsSet) {
+        Set<KnowledgeLevelUnits> userKnowledgeLevelUnits = userData.getKnowledgeLevelUnitsSet();
+        for (KnowledgeLevelUnits knowledgeLevelUnits: knowledgeLevelUnitsSet) {
+            userKnowledgeLevelUnits.add(knowledgeLevelUnits);
+        }
+        userData.setKnowledgeLevelUnitsSet(userKnowledgeLevelUnits);
     }
 }
